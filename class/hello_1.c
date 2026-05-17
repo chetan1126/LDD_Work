6efe59ea1302fb919ca5b186ce36809f78f40403
chetan1126 <chetankotrange11@gmail.com>
2026-05-17 16:18:03 +0530

    new repo hello

---
+++ class/hello_1.c
@@ -0,0 +1,25 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init my_init(void)
+{
+	printk(KERN_INFO"HELLO KERNEL\n");
+	return 0;
+	
+}
+
+static void __exit my_exit(void)
+{
+	printk(KERN_INFO"GOODBYE \n");
+
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("CHETAN");
+MODULE_DESCRIPTION("FIRST KERNEL CODE");
+MODULE_VERSION("1.0");
+
